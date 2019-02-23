07a9325679d6299a83894734957f1777a26393e4
dogppatrick@msn.com <dogppatrick@msn.com>
2019-02-23 15:08:52 +0800

    add catch errro

--- etc_task/main_13g.py
+++ etc_task/main_13g.py
@@ -11,7 +11,7 @@ item_scale = []
 age_class = ["a", "b", "c", "d"]
 c = -1
 q = 0
-with open("data_input.dat", "r") as f:
+with open("input.txt", "r") as f:
     rl = f.read(1)
     subject_age_class = rl
     rl = f.readline()
@@ -21,10 +21,14 @@ with open("data_input.dat", "r") as f:
             item_sum.append(int(0))
             item_count.append(int(0))
             c = c + 1
+            itc = 0
         else:
             if rl != "":
                 item_sum[c] = item_sum[c] + int(rl)
                 item_count[c] = item_count[c] + 1
+                itc = itc + 1
+                if int(rl) > 3 or int(rl) == 0:
+                    print(item_class_des[c], "題目數值錯誤於第", itc, "題出現錯誤\n該題分數為", int(rl), "應為1~3分")
 # read data and count total score
 sc = age_class.index(subject_age_class)
 
@@ -53,30 +57,27 @@ with open("scale_total.txt", "r") as f4:
         if i % 5 == sc+1:
 
             sum_scale.append(int(data_sp[i].split(sep="\t")[sum_scale_raw[q]-sum_item_count[q]]))
-            # print(sum_scale[q])
             q = q + 1
-# for i in range(len(item_scale)):
-#     print("%-15s:%2d%2d" % (item_class_des[i], item_sum[i], item_scale[i]))
+
 with open("output.txt", "w", encoding="utf-8") as f3:
     f3.write("Leiter-R Parent Rating Scale\n")
     f3.write("年齡 class:" + age_class_des[sc] + "\tclass:" + subject_age_class.upper() + "\n")
-    f3.write("領域\t\t\t\t原始分數\t量表\t\n")
+    f3.write("領域\t\t\t原始分數\t量表\t\n")
     for i in range(len(item_class)):
-        # f3.write("%20s:%2d\t%2d\n" % (item_class_des[i], item_sum[i], item_scale[i]))
         f3.write("%-16s" % (item_class_des[i]))
         for j in range(9-len(item_class_des[i])):
             f3.write(" ")
-        f3.write("%-3d\t" % (item_sum[i]))
+        f3.write("%-3d\t\t" % (item_sum[i]))
         if item_scale[i] >= 6:
-            f3.write("%-s*\t\t" % (item_scale[i]))
+            f3.write("%-s*\t\t\t" % (item_scale[i]))
         else:
-            f3.write("%-s\t\t" % (item_scale[i]))
+            f3.write("%-s\t\t\t" % (item_scale[i]))
         f3.write("\n")
-        # f3.write("%-3d\n" % item_count[i])
-    f3.write("量表\t\t\t原始總分\t量尺總分\n")
-    f3.write("認知／社交功能: %3d\t\t\t%3s\n" % (cognitive_social_total, sum_scale[0]))
-    f3.write("情緒／調節:     %3d\t\t\t%3s\n" % (emotions_regulation, sum_scale[1]))
+    f3.write("量表\t\t原始總分\t量尺總分\n")
+    f3.write("認知／社交功能: %3d\t\t%3s\n" % (cognitive_social_total, sum_scale[0]))
+    f3.write("情緒／調節:     %3d\t\t%3s\n" % (emotions_regulation, sum_scale[1]))
     f3.write("註：標註(*)為達臨床顯著性(cutoff point<=6)")
 for i in range(len(item_count)):
     if item_count[i] != check_item[i]:
+        # msg = item_class_des[i], "題數錯誤，目前有:", item_count[i], "\t應有:", check_item[i]
         print(item_class_des[i], "題數錯誤，目前有:", item_count[i], "\t應有:", check_item[i])
